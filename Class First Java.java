fd185b3cd8449e7ffd1fb32432392883e56afe53
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2023-09-14 11:01:13 +0530

    batch

--- Class First Java.java
+++ Class First Java.java
@@ -1,5 +1,6 @@
 class Firstclass{
     public static void main(String args[]) {
-      System.out.println("abhishek kaundal");  
+      System.out.println("abhishek kaundal");
+      System.out.println("Kalvium batch");  
    }
 }    
